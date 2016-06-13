665ab71e8b7a390e40cd69633b7c51dd37041fd5
jayped <jotaped@hotmail.com>
2016-06-14 01:45:09 +0200

    archivo de definicion de la camara del juego

--- PFC/Codigo/include/MakeCamera.h
+++ PFC/Codigo/include/MakeCamera.h
@@ -1,16 +1,32 @@
 #ifndef MAKECAMERA_H
 #define MAKECAMERA_H
 #include <Ogre.h>
+#include <math.h>
+
+#define _posCamX 0
+#define _posCamY 24
+#define _posCamZ 50
+
+using namespace Ogre;
 
 class MakeCamera : public Ogre::Camera
 {
-  
+
+	enum direction {_stop, _up, _down, _right, _left};
+
 public:
 	MakeCamera(const Ogre::String & name, Ogre::SceneManager *creator);
     ~MakeCamera();
-    void init();
-   
-private:
-	
+    
+	void init();
+	bool isMoving();
+	void move();
+	void setCameraDirection(direction aDirection);
+	void setMoving(int aDirection);
+
+protected:
+	bool _isMoving;
+	double _cameraAngle;
+	direction _cameraDirection;
 };
 #endif
