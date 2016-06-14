6cb54e7661d0be575f5eb659a46f102a9dc232c7
jayped <jotaped@hotmail.com>
2016-06-14 17:49:30 +0200

    - El valor de salto se define por tres parametros en X, Y y Z (0,9,0). - Se define el valor de movimiento del personaje. Pasa de .002 cuando estaba en PlayState.cpp a .003. Esto hace al personaje mas rápido y la sensacion de juego es mejor. - se añade la definicion de los métodos move y jump.

--- PFC/Codigo/include/Actor.h
+++ PFC/Codigo/include/Actor.h
@@ -2,10 +2,15 @@
 #define ACTOR_H
 #include <Ogre.h>
 #include <btBulletDynamicsCommon.h>
+#define _jumpX 0 //-15
+#define _jumpY 9
+#define _jumpZ 0
+#define _moveSpeed .003
 
 class Actor : public Ogre::SceneNode
 {
-  
+	enum direction {_stop, _up, _down, _right, _left};
+
 public:
     //Actor(){}
     Actor(Ogre::SceneManager *creator);
@@ -16,6 +21,8 @@ public:
 	void setRigitBody(btRigidBody *myRigitBody);
 	btRigidBody* getRigitBody();
 	void destroyRigitBody();
+	void move(int aDirection);
+	void jump();
 
 private:
 	btRigidBody* _fallRigidBody;
