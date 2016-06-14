66eea16993db12bed241f650616ae2668060d79b
jayped <jotaped@hotmail.com>
2016-06-14 23:14:31 +0200

    - añadido orientate. método para orientar al personaje cuando anda. Se incluye la orientación cuando se mueve en diagonal. - se elimina la orientación del método move.

--- PFC/Codigo/src/Actor.cpp
+++ PFC/Codigo/src/Actor.cpp
@@ -33,37 +33,72 @@ Actor::move(int aDirection)
 	_fallRigidBody->activate(true);
 	btQuaternion btq;
 	btTransform transform;
+
 	// up
 	if (lDirection == _up)
 	{
 		_fallRigidBody->translate(btVector3(0,0,-_moveSpeed));
-		btq = _fallRigidBody->getOrientation();
+		//btq = _fallRigidBody->getOrientation();
 		transform = _fallRigidBody->getWorldTransform();
-		btq.setRotation(btVector3(0,1,0),Ogre::Math::PI);
+		//btq.setRotation(btVector3(0,1,0),Ogre::Math::PI);
 	}
 	if (lDirection == _left)
 	{
 		_fallRigidBody->translate(btVector3(-_moveSpeed,0,0));
-		btq = _fallRigidBody->getOrientation();
+		//btq = _fallRigidBody->getOrientation();
 		transform = _fallRigidBody->getWorldTransform();
-		btq.setRotation(btVector3(0,1,0),(3.0/2.0) * Ogre::Math::PI);
+		//btq.setRotation(btVector3(0,1,0),(3.0/2.0) * Ogre::Math::PI);
 	}
 	if (lDirection == _down)
 	{
 		_fallRigidBody->translate(btVector3(0,0,_moveSpeed));
-		btq = _fallRigidBody->getOrientation();
+		//btq = _fallRigidBody->getOrientation();
 		transform = _fallRigidBody->getWorldTransform();
-		btq.setRotation(btVector3(0,1,0),0);
+		//btq.setRotation(btVector3(0,1,0),0);
 	}
 	if (lDirection == _right)
 	{
 		_fallRigidBody->translate(btVector3(_moveSpeed,0,0));
-		btq = _fallRigidBody->getOrientation();
+		//btq = _fallRigidBody->getOrientation();
 		transform = _fallRigidBody->getWorldTransform();
-		btq.setRotation(btVector3(0,1,0),Ogre::Math::PI/2);
+		//btq.setRotation(btVector3(0,1,0),Ogre::Math::PI/2);
 	}
 
+
+	//transform.setRotation(btq);
+	_fallRigidBody->activate(true);
+	_fallRigidBody->setWorldTransform(transform);
+}
+
+void
+Actor::orientate(bool aRotate[])
+{
+	btQuaternion btq;
+	btTransform transform;
+	Ogre::Real lRotate=0;
+
+	// down
+	if ( (aRotate[0]) && !(aRotate[1]) && !(aRotate[2]) && !(aRotate[3])) lRotate = 0;
+	// down right
+	if ( (aRotate[0]) && (aRotate[1]) && !(aRotate[2]) && !(aRotate[3])) lRotate = Ogre::Math::PI/4;
+	// down left
+	if ( (aRotate[0]) && !(aRotate[1]) && !(aRotate[2]) && (aRotate[3])) lRotate = (7.0/4.0) * Ogre::Math::PI;
+	// right
+	if ( !(aRotate[0]) && (aRotate[1]) && !(aRotate[2]) && !(aRotate[3])) lRotate = Ogre::Math::PI/2;
+	// right up
+	if ( !(aRotate[0]) && (aRotate[1]) && (aRotate[2]) && !(aRotate[3])) lRotate = (3.0/4.0) * Ogre::Math::PI;
+	// up
+	if ( !(aRotate[0]) && !(aRotate[1]) && (aRotate[2]) && !(aRotate[3])) lRotate = Ogre::Math::PI;
+	// up left
+	if ( !(aRotate[0]) && !(aRotate[1]) && (aRotate[2]) && (aRotate[3])) lRotate = (5.0/4.0) * Ogre::Math::PI;
+	// left
+	if ( !(aRotate[0]) && !(aRotate[1]) && !(aRotate[2]) && (aRotate[3])) lRotate = (3.0/2.0) * Ogre::Math::PI;
+	
+	btq = _fallRigidBody->getOrientation();
+	btq.setRotation(btVector3(0,1,0),lRotate);
+	transform = _fallRigidBody->getWorldTransform();
 	transform.setRotation(btq);
+
 	_fallRigidBody->activate(true);
 	_fallRigidBody->setWorldTransform(transform);
 }
