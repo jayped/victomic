bc19b2558d6f7e7b6c461932c17864d762b8dd75
jayped <jotaped@hotmail.com>
2016-06-14 17:37:31 +0200

    Añadidos los métodos:  - move. Mueve al actor (personaje principal y enemigos) para una dirección concreta. Esta dirección será normalizada con respecto a la orientación de la camara en posteriores versiones.  - jump. Efectua un salto con el actor.

--- PFC/Codigo/src/Actor.cpp
+++ PFC/Codigo/src/Actor.cpp
@@ -26,3 +26,60 @@ Actor::getRigitBody()
 	return _fallRigidBody;
 }
 
+void
+Actor::move(int aDirection)
+{
+	direction lDirection = (direction) aDirection;
+	_fallRigidBody->activate(true);
+	btQuaternion btq;
+	btTransform transform;
+	// up
+	if (lDirection == _up)
+	{
+		_fallRigidBody->translate(btVector3(0,0,-_moveSpeed));
+		btq = _fallRigidBody->getOrientation();
+		transform = _fallRigidBody->getWorldTransform();
+		btq.setRotation(btVector3(0,1,0),Ogre::Math::PI);
+	}
+	if (lDirection == _left)
+	{
+		_fallRigidBody->translate(btVector3(-_moveSpeed,0,0));
+		btq = _fallRigidBody->getOrientation();
+		transform = _fallRigidBody->getWorldTransform();
+		btq.setRotation(btVector3(0,1,0),(3.0/2.0) * Ogre::Math::PI);
+	}
+	if (lDirection == _down)
+	{
+		_fallRigidBody->translate(btVector3(0,0,_moveSpeed));
+		btq = _fallRigidBody->getOrientation();
+		transform = _fallRigidBody->getWorldTransform();
+		btq.setRotation(btVector3(0,1,0),0);
+	}
+	if (lDirection == _right)
+	{
+		_fallRigidBody->translate(btVector3(_moveSpeed,0,0));
+		btq = _fallRigidBody->getOrientation();
+		transform = _fallRigidBody->getWorldTransform();
+		btq.setRotation(btVector3(0,1,0),Ogre::Math::PI/2);
+	}
+
+	transform.setRotation(btq);
+	_fallRigidBody->activate(true);
+	_fallRigidBody->setWorldTransform(transform);
+}
+
+void
+Actor::jump()
+{
+	btVector3 impulse(_jumpX,_jumpY,_jumpZ);
+	_fallRigidBody->activate(true);
+	_fallRigidBody->applyCentralImpulse(impulse);
+}
+
+/*
+void
+destroyRigitBody()
+{
+	//_fallRigidBody
+}*/
+
