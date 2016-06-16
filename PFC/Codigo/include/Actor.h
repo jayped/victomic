4724abfc77c5acbc736a142f06b67f257bad8b2e
jayped <jotaped@hotmail.com>
2016-06-16 23:30:41 +0200

    - cambiada la velocidad del actor para hacerlo mas rapido en resoluciones altas - añadido el estado del personaje, para saber en que momento esta parado, saltando o cayendo.

--- PFC/Codigo/include/Actor.h
+++ PFC/Codigo/include/Actor.h
@@ -5,18 +5,19 @@
 #define _jumpX 0 //-15
 #define _jumpY 9
 #define _jumpZ 0
-#define _moveSpeed .004 // .003 sin sombras
+#define _moveSpeed .01 // .003 sin sombras .004 800x600. hay que hacerlo respecto de los frames
 
 class Actor : public Ogre::SceneNode
 {
 	enum direction {_stop, _up, _down, _right, _left};
-	
+
 public:
     //Actor(){}
     Actor(Ogre::SceneManager *creator);
     Actor(Ogre::SceneManager *creator, const Ogre::String & name);
     ~Actor();
     
+	enum states {_stay, _jumping, _falling};
 	void init();
 	void setRigitBody(btRigidBody *myRigitBody);
 	btRigidBody* getRigitBody();
@@ -25,10 +26,12 @@ public:
 	void orientate(bool aRotate[], int aCameraPosition);
 	void jump();
 	void resetSpeed();
+	states getState();
 
 private:
 	btRigidBody* _fallRigidBody;
 	double _currentSpeed;
+	states _state;
 	
 };
 #endif
