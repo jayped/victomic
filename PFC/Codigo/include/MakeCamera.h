b9717b9ab2c8ba9ab778d4067557b28b21382af7
jayped <jotaped@hotmail.com>
2016-06-18 19:58:16 +0200

    - Se modifica la posición de la cámara. - Se añade _cameraDistance para controlar la distancia de la cámara a la acción. Ademas se añade el método setCameraDistance que no será usado en el juego y sí durante los test.

--- PFC/Codigo/include/MakeCamera.h
+++ PFC/Codigo/include/MakeCamera.h
@@ -4,8 +4,8 @@
 #include <math.h>
 
 #define _posCamX 0
-#define _posCamY 24
-#define _posCamZ 50
+#define _posCamY 19
+#define _posCamZ 40
 
 #define _cameraMoveSpeed 200
 
@@ -26,12 +26,13 @@ public:
 	void setCameraDirection(direction aDirection);
 	void setMoving(int aDirection);
 	int getCameraPosition();
+	void setCameraDistance(int aCameraDistance);
 
 protected:
 	bool _isMoving;
 	double _cameraAngle;
 	direction _cameraDirection;
 	int _cameraPosition; // 4 posiciones posibles en las 4 caras del cubo.
-
+	int _cameraDistance;
 };
 #endif
