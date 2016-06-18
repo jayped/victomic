714a1289aa69a8cb8a9837a51d04015717eeba0d
jayped <jotaped@hotmail.com>
2016-06-18 19:55:07 +0200

    - Bug Fixed. La cámara, al cambiar su movimiento a relativo al tiempo, a veces recibe en el metodo move una aSpeedRelative igual a 0 lo que desencadena un fallo al multiplicar esta por el angulo de la cámara. Para evitar este problema que hace que la camara, a veces no se mueva o se mueva de golpe (si esta el ultimo bloque de codigo incluido), se añade un pequeño incremento a aSpeedRelative, al inicio del método.
    - Se añade el metodo setCameraDistance para poder hacer calculos y giros con la cámara, a la distancia a la que se encuentre. Esto no tendrá uso en el juego.

--- PFC/Codigo/src/MakeCamera.cpp
+++ PFC/Codigo/src/MakeCamera.cpp
@@ -18,6 +18,7 @@ MakeCamera::init()
 	_cameraAngle = 0;
 	_cameraDirection = _stop;
 	_cameraPosition = 0;
+	_cameraDistance = _posCamZ;
 }
 
 void
@@ -57,17 +58,29 @@ MakeCamera::getCameraPosition()
 }
 
 void
+MakeCamera::setCameraDistance(int aCameraDistance)
+{
+	_cameraDistance=aCameraDistance;
+}
+
+
+void
 MakeCamera::move(double aSpeedRelative)
 {
+		// En ejecuciones a baja resolucion, a veces llegan velocidades a 0.0 y esto hace que la camara no se mueva
+		// y salte de golpe a la siguiente posicion fija. De esta forma, fallarian los calculos de la camara
+		// y provocaria fallos en movimiento del personaje. con el forzado de velocidad positiva, se soluciona.
+		aSpeedRelative+=0.00001;
+
 		if (_cameraDirection==_right)
 			_cameraAngle+=_cameraMoveSpeed*aSpeedRelative;//0.4; // 0.1 sin sombras 0.15 800x600. hay que hacerlo respecto de los frames
 		else if (_cameraDirection==_left)
 			_cameraAngle-=_cameraMoveSpeed*aSpeedRelative;//0.4; // 0.1 sin sombras 0.15 800x600. hay que hacerlo respecto de los frames
-		
-		double posx = (Math::Sin(Math::AngleUnitsToRadians(_cameraAngle))*50);
-		double posz = (Math::Cos(Math::AngleUnitsToRadians(_cameraAngle))*50);
 
-		this->setPosition(Ogre::Vector3(posx,_posCamY,posz));
+		double posx = (Math::Sin(Math::AngleUnitsToRadians(_cameraAngle))*/*_cameraDistance*/_cameraDistance);
+		double posz = (Math::Cos(Math::AngleUnitsToRadians(_cameraAngle))*/*_cameraDistance*/_cameraDistance);
+
+		this->setPosition(Ogre::Vector3(posx,this->getPosition().y,posz));
 		double modulo;
 
 		if (_cameraDirection==_right)
@@ -75,7 +88,6 @@ MakeCamera::move(double aSpeedRelative)
 		else if (_cameraDirection==_left)
 			modulo = (int)Math::Floor(_cameraAngle) % 90;
 		
-		//if ( (_cameraAngle<(90.0+0.05)) && (_cameraAngle>(90.0-0.05)) )
 		if ( (modulo==0) )
 		{
 			if (_cameraDirection==_right)
@@ -83,6 +95,15 @@ MakeCamera::move(double aSpeedRelative)
 			else if (_cameraDirection==_left)
 				_cameraAngle = Math::Floor(_cameraAngle);
 			_isMoving=false;
+		}
+		
+		// centrado de la camara para quitar el epsilon.
+		if (!_isMoving)
+		{
+			if (_cameraPosition==0)this->setPosition(Ogre::Vector3(0,this->getPosition().y,_cameraDistance));
+			if (_cameraPosition==1)this->setPosition(Ogre::Vector3(_cameraDistance,this->getPosition().y,0));
+			if (_cameraPosition==2)this->setPosition(Ogre::Vector3(0,this->getPosition().y,-_cameraDistance));
+			if (_cameraPosition==3)this->setPosition(Ogre::Vector3(-_cameraDistance,this->getPosition().y,0));
 		}
 }
 
