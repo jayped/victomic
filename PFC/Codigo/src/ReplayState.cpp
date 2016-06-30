41e2e21756695867da85cf14a8ab7aac901b6eca
jayped <jotaped@hotmail.com>
2016-06-30 23:43:32 +0200

    - carga el mapa de progreso y lo ajusta a la pantalla guardando el ratio de aspecto de la imagen del mapa.

--- PFC/Codigo/src/ReplayState.cpp
+++ PFC/Codigo/src/ReplayState.cpp
@@ -6,6 +6,7 @@ template<> ReplayState* Ogre::Singleton<ReplayState>::msSingleton = 0;
 void
 ReplayState::enter ()
 {
+	bool mapFit;
 	if (!_root)
 	{
 	  _root = Ogre::Root::getSingletonPtr();
@@ -20,10 +21,28 @@ ReplayState::enter ()
 	  // Nuevo background colour.
 	  //_viewport->setBackgroundColour(Ogre::ColourValue(0.0, 1.0, 1.0));
 	  _overlayMgr = Ogre::OverlayManager::getSingletonPtr();
+
+	  mapFit=false;
 	}
 
-  _overlayMgr = Ogre::OverlayManager::getSingletonPtr();
-  overlay = _overlayMgr->getByName("Replay");
+	_overlayMgr = Ogre::OverlayManager::getSingletonPtr();
+	overlay = _overlayMgr->getByName("Avance");
+	Ogre::OverlayElement *mapOverlay = _overlayMgr->getOverlayElement("avance");
+
+	if (mapFit==false)
+	{
+		mapFit=true;
+		double width = _viewport->getActualWidth();
+		double height = _viewport->getActualHeight();
+		double aspecRatioPanel = mapOverlay->getWidth()/mapOverlay->getHeight();
+		double reduction = 0.8;
+
+		mapOverlay->setDimensions(height*aspecRatioPanel*reduction,height*reduction);
+		mapOverlay->setPosition(-(mapOverlay->getWidth()/2),-mapOverlay->getHeight()/2);
+
+	}
+	
+/*
   _ReplayOverlay = _overlayMgr->getOverlayElement("reTitleLabel");
   _ReplayOverlay->setCaption("Game results:");
   _ReplayOverlay = _overlayMgr->getOverlayElement("reScoreLabel");
@@ -36,9 +55,10 @@ ReplayState::enter ()
   //_ReplayOverlay->setCaption(Ogre::StringConverter::toString(_gameMgr->_hiscore));
   _ReplayOverlay = _overlayMgr->getOverlayElement("reInfoLabel");
   _ReplayOverlay->setCaption("PRESS SPACE TO REPLAY...");
+  */
 
   overlay->show();
-  _ReplayOverlay->show();
+  //_ReplayOverlay->show();
 	//_gameMgr->playEnd();
 
   _exitGame = false;
@@ -48,7 +68,7 @@ void
 ReplayState::exit ()
 {
   overlay->hide();
-  _ReplayOverlay->hide();
+  //_ReplayOverlay->hide();
   	_gameMgr->playMenu();
 
 }
